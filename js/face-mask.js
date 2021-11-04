7669cb65b5a399f092eb39c75bfb156e26dead84
aibots <leandrocg10@gmail.com>
2021-11-04 16:11:35 -0600

    Update face-mask.js

--- js/face-mask.js
+++ js/face-mask.js
@@ -10,7 +10,7 @@ let detectFace = false;
 let clearMask = false;
 let maskOnImage = false;
 let masks = [];
-let maskKeyPointIndexs = [0, 17, 185, 409]; //lips
+let maskKeyPointIndexs = [10, 152, 234, 454]; //lips
 
 
 $("#webcam-switch").change(function () {
